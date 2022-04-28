37b64361a838e5a1574ac64fda00ca503fc8848e
Tyler Soriano <soriano.tyler@gmail.com>
2022-04-27 22:18:29 -0700

    single point code

--- static/js/logic.js
+++ static/js/logic.js
@@ -32,3 +32,17 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
+
+ L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+ // We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
